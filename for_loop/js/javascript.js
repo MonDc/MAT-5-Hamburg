6a5abaa0e07b83cf987c1b35afc28b59ce1e31bc
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2018-10-22 15:06:51 +0200

    loop inside loop

--- for_loop/js/javascript.js
+++ for_loop/js/javascript.js
@@ -4,6 +4,43 @@ function doSomthing(){
         //alert("the number is :"+i);
 
 
-        document.getElementById("container").innerHTML=document.getElementById("container").innerHTML+"Hamburg mat 5 "+i+"<br>";
+        document.getElementById("container").innerHTML=
+        document.getElementById("container").innerHTML+
+        "Hamburg mat 5 "+i+"<br>";
+    }
+}
+function counter(){
+    document.getElementById("result").innerHTML="";
+    for(let i=0;i<=100;i++){
+        document.getElementById("result").innerHTML += i+"<br>";
+    }
+}
+function counterEven(){
+    document.getElementById("result").innerHTML="";
+    for(let i=0;i<=100;i=i+2){
+        document.getElementById("result").innerHTML += i+"<br>";
+    }
+}
+function counterOdd(){
+    document.getElementById("result").innerHTML="";
+    for(let i=1;i<=100;i=i+2){
+        document.getElementById("result").innerHTML += i+"<br>";
+    }
+}
+function power(){
+    document.getElementById("result").innerHTML="";
+    for(let i=1;i<=100;i++){
+        document.getElementById("result").innerHTML += i*i+"<br>";
+    }
+}
+function innerLoop(){
+    document.getElementById("result").innerHTML="";
+    for(let i=1;i<=10;i++){
+        for(let x=1 ;x<=10 ; x++){
+            document.getElementById("result").innerHTML += "i is"+i+" and x is "+x+"<br>";
+        }
+
+
+
     }
 }
