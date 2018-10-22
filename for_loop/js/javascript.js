2fb9825b1de3044dd8591f24d205c73710e8edcd
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2018-10-22 13:25:07 +0200

    for loop

---
+++ for_loop/js/javascript.js
@@ -0,0 +1,9 @@
+
+function doSomthing(){
+    for(let i = 0; i<10 ; i++){
+        //alert("the number is :"+i);
+
+
+        document.getElementById("container").innerHTML=document.getElementById("container").innerHTML+"Hamburg mat 5 "+i+"<br>";
+    }
+}
