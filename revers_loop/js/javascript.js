6d896112b6d2b0230951aad07f10f4f83fc765a3
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2018-10-23 10:14:48 +0200

    Revers Loop

---
+++ revers_loop/js/javascript.js
@@ -0,0 +1,6 @@
+function normalLoop(){
+    document.getElementById("result").innerHTML="";
+    for(let i=10 ; i>=0 ; i--){
+        document.getElementById("result").innerHTML +=i+"<br>" ;
+    }
+}
