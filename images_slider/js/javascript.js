4190062a3fbc1fbf658a57ebaf8a22e18e142364
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2018-10-23 12:18:51 +0200

    images slide with moving

--- images_slider/js/javascript.js
+++ images_slider/js/javascript.js
@@ -1,7 +1,31 @@
 window.onload=function() {
     //everything here wil be don after page load
+    let counter=1;
+    let counter1=1;
+    /////////////////////////////////to change image
     setInterval(function(){
 //everything here will be repeated 
-document.getElementById("slideImage").src="./imgs/2.jpg";
-    },3000);
+//counter++;
+//counter += 1;
+counter = counter +1;
+if(counter == 6){
+    counter=1;
+}
+document.getElementById("slideImage").src="./imgs/"+counter+".jpg";
+    },1000);
+//////////////////////////////// finish change image
+
+///////////////////////////////change left position
+
+    setInterval(function(){
+        //everything here will be repeated 
+        //counter++;
+        //counter += 1;
+        counter1 = counter1 +1;
+        if(counter1 ==500){
+            counter1=1;
+        }
+        document.getElementById("slideImage").style="width: 200px;position: fixed;left: "+counter1+"px;";
+            },1);
+            ////////////////////////////////////////finish change position
 }
