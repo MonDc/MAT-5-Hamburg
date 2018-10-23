82f42033cc28ecc0613a8700e16bac481e6b5c03
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2018-10-23 11:24:21 +0200

    Image Slider

---
+++ images_slider/js/javascript.js
@@ -0,0 +1,7 @@
+window.onload=function() {
+    //everything here wil be don after page load
+    setInterval(function(){
+//everything here will be repeated 
+document.getElementById("slideImage").src="./imgs/2.jpg";
+    },3000);
+}
--- revers_loop/js/javascript.js
+++ revers_loop/js/javascript.js
@@ -4,3 +4,15 @@ function normalLoop(){
         document.getElementById("result").innerHTML +=i+"<br>" ;
     }
 }
+
+function reversTriangle()
+{
+    document.getElementById("result").innerHTML="";
+    for(let i=5;i>=1;i--){
+        for(let f=1;f<=i;f++)
+        {
+            document.getElementById("result").innerHTML +="*" ;
+        }
+        document.getElementById("result").innerHTML +="<br>" ;
+    }
+}
