076691494336bb2301d2e19e57966e8222afc5b8
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2018-10-23 14:03:00 +0200

    array memmory use

---
+++ array/js/javascript.js
@@ -0,0 +1,25 @@
+window.onload=function(){
+let arr=[2,3,0,1,6];
+//alert(arr.length);// show the count of array's items
+//alert(arr[0]);// show the first item on the array
+for(let i = 0 ; i<arr.length ; i++){
+    //alert(arr[i]);
+}
+
+};
+
+let numbers=[];
+let idx=0;
+
+function calc(){
+let inputNumber=parseFloat( document.getElementById("markInput").value);
+numbers[idx]=inputNumber;
+idx++;
+}
+
+function show(){
+    document.getElementById("result").innerHTML="";
+    for(let i=0;i<numbers.length;i++){
+        document.getElementById("result").innerHTML +=numbers[i]+"<br>";
+    }
+}
