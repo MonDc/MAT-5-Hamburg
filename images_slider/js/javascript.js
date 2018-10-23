896ec11f3bb49c29825e370d039de95e21471733
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2018-10-23 12:58:58 +0200

    last version of image slider

--- images_slider/js/javascript.js
+++ images_slider/js/javascript.js
@@ -1,7 +1,8 @@
 window.onload=function() {
     //everything here wil be don after page load
     let counter=1;
-    let counter1=1;
+    let counter1=0;
+    let direction = true;
     /////////////////////////////////to change image
     setInterval(function(){
 //everything here will be repeated 
@@ -21,9 +22,22 @@ document.getElementById("slideImage").src="./imgs/"+counter+".jpg";
         //everything here will be repeated 
         //counter++;
         //counter += 1;
-        counter1 = counter1 +1;
-        if(counter1 ==500){
-            counter1=1;
+        
+        // check if the left position equel to 
+        //the window width minus 200 the image width 
+        //then the direction should revers
+        if(counter1 ==window.innerWidth-200){
+            direction=false;
+        }
+        //if counter arrave to 0 the direction should revers
+        if(counter1 ==0){
+            direction=true;
+        }
+        if(direction)
+        {
+            counter1 = counter1 +1;
+        }else{
+            counter1 = counter1 -1;
         }
         document.getElementById("slideImage").style="width: 200px;position: fixed;left: "+counter1+"px;";
             },1);
