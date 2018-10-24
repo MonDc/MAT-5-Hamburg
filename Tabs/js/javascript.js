e8ee9622a6e6491d3f6389a9d4b911d596da6e44
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2018-10-24 12:13:43 +0200

    show hide divs Tabs

--- Tabs/js/javascript.js
+++ Tabs/js/javascript.js
@@ -1,4 +1,24 @@
 function homeClick(){
     document.getElementById("homeDiv").style="";
     document.getElementById("galleryDiv").style="display: none;";
+    document.getElementById("aboutDiv").style="display: none;";
+    document.getElementById("contactDiv").style="display: none;";
+}
+function aboutClick(){
+    document.getElementById("aboutDiv").style="";
+    document.getElementById("galleryDiv").style="display: none;";
+    document.getElementById("homeDiv").style="display: none;";
+    document.getElementById("contactDiv").style="display: none;";
+}
+function galleryClick(){
+    document.getElementById("aboutDiv").style="display: none;";
+    document.getElementById("galleryDiv").style="";
+    document.getElementById("homeDiv").style="display: none;";
+    document.getElementById("contactDiv").style="display: none;";
+}
+function contactClick(){
+    document.getElementById("aboutDiv").style="display: none;";
+    document.getElementById("galleryDiv").style="display: none;";
+    document.getElementById("homeDiv").style="display: none;";
+    document.getElementById("contactDiv").style="";
 }
