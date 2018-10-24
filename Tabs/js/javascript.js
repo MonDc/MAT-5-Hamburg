eeb59b26a7489d21c7533978070eff78a5505805
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2018-10-24 14:06:47 +0200

    tabs header active

--- Tabs/js/javascript.js
+++ Tabs/js/javascript.js
@@ -1,22 +1,26 @@
-function homeClick(){
+function homeClick(i){
+    i.parentNode.classList.add("active");
     document.getElementById("homeDiv").style="";
     document.getElementById("galleryDiv").style="display: none;";
     document.getElementById("aboutDiv").style="display: none;";
     document.getElementById("contactDiv").style="display: none;";
 }
-function aboutClick(){
+function aboutClick(me){
+    me.parentNode.classList.add("active");
     document.getElementById("aboutDiv").style="";
     document.getElementById("galleryDiv").style="display: none;";
     document.getElementById("homeDiv").style="display: none;";
     document.getElementById("contactDiv").style="display: none;";
 }
-function galleryClick(){
+function galleryClick(you){
+    you.parentNode.classList.add("active");
     document.getElementById("aboutDiv").style="display: none;";
     document.getElementById("galleryDiv").style="";
     document.getElementById("homeDiv").style="display: none;";
     document.getElementById("contactDiv").style="display: none;";
 }
-function contactClick(){
+function contactClick(him){
+    him.parentNode.classList.add("active");
     document.getElementById("aboutDiv").style="display: none;";
     document.getElementById("galleryDiv").style="display: none;";
     document.getElementById("homeDiv").style="display: none;";
